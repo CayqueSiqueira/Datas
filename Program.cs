174758938e7abd9886e2eb2c522c35876bc3dd95
CayqueSiqueira <cayquesm@gmail.com>
2025-03-17 23:33:40 -0300

    Organizando e comentando o codigo

--- Program.cs
+++ Program.cs
@@ -9,21 +9,26 @@ namespace Datas
         {
             Console.Clear();
 
-            // var data = new DateTime();//1/1/0001 12:00:00 AM
-            // var data2 = DateTime.Now;//3/16/2025 1:20:34 AM
-            // var data3 = new DateTime(2020, 10, 12, 8, 23, 14);//datetime com ano mes dia hora minuto e segundo definidos
-            // Console.WriteLine(data);
-            // Console.WriteLine(data2);
-            // Console.WriteLine(data3);
-            // Console.WriteLine(data3.Year);
-            // Console.WriteLine(data3.Month);
-            // Console.WriteLine(data3.Day);
-            // Console.WriteLine(data3.DayOfWeek);
-            // Console.WriteLine(data3.DayOfYear);
-            // Console.WriteLine(data3.Second);
+            //Datas
+             var data = new DateTime();// a data é uma estrutura que tem um valor padrão
+             Console.WriteLine(data);
 
+             var data2 = DateTime.Now;//Pegar a data atual
+             Console.WriteLine(data2);
+             Console.WriteLine();
+
+            //Obtendo valores da data
+            var data3 = new DateTime(2025, 11, 15, 8, 10, 01);//datetime com ano mes dia hora minuto e segundo definidos
+            Console.WriteLine(data3);
+            Console.WriteLine(data3.Year +"/"+ data3.Month+"/"+data3.Day+" "+data3.Hour+":"+data3.Minute+":"+data3.Second);
+            Console.WriteLine(data3.DayOfWeek);
+            Console.WriteLine((int)data3.DayOfWeek);
+            Console.WriteLine(data3.DayOfYear);
+            Console.WriteLine(data3.Second);
+            Console.WriteLine();
+
+            //Formatando dados
             var data4 = DateTime.Now;
-            //formatação de data
             // M - mes \ m - minuto \ d - dia \ y - ano \ h - hora \ s - segundo \ f - fração de segundo \ Y - dia do ano
             var formatada = String.Format("{0:yyyy-mm-dd}",data4);
             Console.WriteLine(formatada);
@@ -33,7 +38,7 @@ namespace Datas
             var formatada3 = String.Format("{0:dd-mm-yyyy hh-mm-ss}",data4);
             Console.WriteLine(formatada3);
 
-            var formatada4 = String.Format("{0:dd-mm-yyyy hh-mm-ss ff zz} ",data4);
+            var formatada4 = String.Format("{0:dd-mm-yyyy hh-mm-ss ff zz} ",data4);//zz time zone
             Console.WriteLine(formatada4);
         }
     }
